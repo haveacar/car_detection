fc7b797aaab2010a1d4b804dc67e606e0dfc17a4
DanielZh <haveacar.zhovnir@gmail.com>
2024-02-01 21:07:59 +0200

    update detection

--- controls.py
+++ controls.py
@@ -1,11 +1,26 @@
+import json
 
 import cv2
 import os
 from ultralytics import YOLO
 
+def upload_configuration() -> dict:
+    """
+    Function to upload basic settings from a JSON file.
+    """
+    path = os.path.join(os.path.dirname(__file__), 'settings.json')
+    with open(path) as f:
+        data = json.load(f)
+
+    return data
+
 
 class CarDetector:
-    def __init__(self, yolo_name, link):
+    def __init__(self, yolo_name, link, conf):
+        # detection names
+        self.names = ('car', 'motorbike', 'bus', 'truck', 'person', 'dog', 'bicycle')
+        # detection
+        self.conf = conf
         # path
         path = os.path.join(os.path.dirname(__file__), yolo_name)
         # load yolo model
@@ -15,7 +30,7 @@ class CarDetector:
         # initialize detecting
         self.stream_detect()
 
-    def stream_detect(self):
+    def stream_detect(self)->None:
         # Loop through the video frames
         while self.cap.isOpened():
             # Read a frame from the video
@@ -23,7 +38,14 @@ class CarDetector:
 
             if success:
                 # Run YOLOv8 tracking on the frame, persisting tracks between frames
-                results = self.model.track(frame, persist=True)
+                results = self.model.track(frame, persist=True, conf=0.2)
+
+                # detected data processing
+
+                detected_name = results[0].tojson()
+
+                if detected_name:
+                    self._detection_procesing(json.loads(detected_name))
 
                 # Visualize the results on the frame
                 annotated_frame = results[0].plot()
@@ -42,8 +64,13 @@ class CarDetector:
         self.cap.release()
         cv2.destroyAllWindows()
 
+    def _detection_procesing(self, detected:list[dict])->None:
 
-LINK = "http://185.146.206.159:80/cgi-bin/faststream.jpg?stream=half&fps=15&rand=COUNTER"
-# load_stream_video(LINK)
+       if detected:
+           for object in detected:
+               if object.get('name') in self.names:
+                   print('********', object)
 
-detector = CarDetector("yolov8s.pt", LINK)
+
+
+
